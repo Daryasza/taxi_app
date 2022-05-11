4bf6b5353b9063bd2c4c1ef8c17ea9a3746ed757
Darya Sharnevich <darya.sharnevich@gmail.com>
2022-05-11 20:36:41 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
